2cf1749e843966ce513ed301404556132264da2e
Rob <lastres.moya.53@gmail.com>
2017-08-20 22:48:09 -0500

    initial Meal PLanner with meals and recipes

--- src/components/common/Meal/index.js
+++ src/components/common/Meal/index.js
@@ -1,57 +1,80 @@
-import React, { Component } from 'react';
+import React from 'react';
+import {connect} from 'react-redux';
+import createReactClass from 'create-react-class'
 import PropTypes from 'prop-types';
+import Recipe from '../Recipe';
 
-class Meal extends Component {
-    render() {
+const Meal = createReactClass( {
+    componentWillMount: function() {
+        // console.log(this.props);
+    },
+    render: function() {
+
+        const { nutrients, recipePlans } = this.props.meal;
+
+        let renderNutrients = () => {
+            return Object.keys(nutrients).map(function(key, i) {
+                return <small key={i}>{key}: {nutrients[key].toFixed(2)} &nbsp;</small>;
+            })
+        }
+        let renderRecipes = () => {
+            return recipePlans.map((recipePlan, i) => {
+                return (
+                    <Recipe key={i} recipe={recipePlan} />
+                )
+            })
+        }
         return(
-            <div className="meal-card-container">
-                <div>
-                    <h3 className="margin-top-none">Meal
+            <div>
+                <div className="card">
+                    <div className="card-body">
+                        <h3 className="margin-top-none">Meal &nbsp;
+                        <small>{nutrients.calories.toFixed(2)} Calories</small>
+                            {/*
+                                <small className="margin-left-md text-light-black">500 Calories
+                                    <i className="fa fa-random margin-left-md"></i>
+                                    <i className="fa fa-ellipsis-h margin-left-md"></i>
+                                </small>
+                                */}
+
+                        </h3>
+                        {renderNutrients()}
+                        <div className="card">
+                            <ul className="list-group list-group-flush">
+                                {renderRecipes()}
+                            </ul>
+                        </div>
                         {/*
-                            <small className="margin-left-md text-light-black">500 Calories
-                                <i className="fa fa-random margin-left-md"></i>
-                                <i className="fa fa-ellipsis-h margin-left-md"></i>
-                            </small>
+                            <span className="pull-right text-red margin-top-sm">
+                                <i className="icon-heart margin-right-lg"></i>
+                                <i className="icon-carrot margin-right-lg"></i>
+                                <i className="icon-gluten"></i>
+                            </span>
                             */}
-
-                    </h3>
-                    {/**
-                        <p>
-                            <small>Calories {nutrients.calories.toFixed(2)} &nbsp;</small>
-                            <small>Protein {nutrients.protein.toFixed(2)} &nbsp;</small>
-                            <small>Fat {nutrients.fat.toFixed(2)} &nbsp;</small>
-                            <small>Carbohydrates {nutrients.carbohydrates.toFixed(2)} &nbsp;</small>
-                            <small>Fiber {nutrients.fiber.toFixed(2)}</small>
-                        </p>
-                        /}
-
-                    {/*
-                        <span className="pull-right text-red margin-top-sm">
-                            <i className="icon-heart margin-right-lg"></i>
-                            <i className="icon-carrot margin-right-lg"></i>
-                            <i className="icon-gluten"></i>
-                        </span>
-                        */}
-                </div>
-                <div className="clearfix"></div>
-
-
-
-                <div className="meal-card-wrapper">
-                    {/*{renderRecipes()}*/}
-
+                    </div>
 
                 </div>
+                <br/>
             </div>
         )
     }
-}
+});
 
 Meal.propTypes = {
-    meal: PropTypes.shape({
-        recipes: PropTypes.array.isRequired,
-        nutrients: PropTypes.object.isRequired
-    }).isRequired
+    meal: PropTypes.object.isRequired
 }
 
-export default Meal;
+const mapStateToProps = (state) => {
+    return {
+    };
+}
+const mapDispatchToProps = (dispatch) => {
+    return {
+        // onMount: () => {
+            // dispatch(fetchMealPlan({"mealsPerDay": 2, "recipesPerMeal": 3}));
+            // dispatch(actions.createMealPlan({"mealsPerDay": 2, "recipesPerMeal": 3}));
+        // }
+    };
+};
+
+export default connect(mapStateToProps,mapDispatchToProps)(Meal);
