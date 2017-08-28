5c58e84071d3eaffdca8e16ef89534b08c07a12e
Rob <lastres.moya.53@gmail.com>
2017-08-28 16:12:14 -0500

    mvp page

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ import { Provider } from 'react-redux';
 import registerServiceWorker from './registerServiceWorker';
 import configureStore from './core/store/configureStore';
 import { createBrowserHistory } from 'history'
-import HomePage from './components/HomePage';
+// import HomePage from './components/HomePage';
+import MvpPage from './components/MvpPage';
 import MealPlanner from './components/MealPlanner';
 import 'font-awesome/css/font-awesome.css';
 import './base.css';
@@ -23,7 +24,7 @@ ReactDOM.render(
     <Provider store={store}>
         <Router history={history}>
             <div>
-                <Route exact path="/" component={HomePage}></Route>
+                <Route exact path="/" component={MvpPage}></Route>
                 <Route exact path="/mealplanner" component={MealPlanner}></Route>
             </div>
         </Router>
